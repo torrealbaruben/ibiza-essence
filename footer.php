c231e93f7db66e909dc7213fd1fe6a06c42182d4
Ruben Torrealba <torrealbaruben@gmail.com>
2025-04-01 17:31:13 -0400

    Footer

--- footer.php
+++ footer.php
@@ -1,3 +1,59 @@
+	<footer class="">
+		<div class="container">
+			<div class="row footer">
+				<div class="col-12">
+					<ul class="info">
+						<li><a href="#">+1 123 456.47890</a></li>
+						<li><a href="#">hello@ibiozaessence.com</a></li>
+						<li><a href="#">@ibiozaessence</a></li>
+					</ul>
+				</div>
+				<div class="col-12 col-sm-6 col-lg-3">
+					<ul class="menu">
+						<li>Hospitality</li>
+						<li><a href="#">Reservations hoteles</a></li>
+						<li><a href="#">Villas venta</a></li>
+						<li><a href="#">Villas alquiler</a></li>
+						<li><a href="#">Pisos venta</a></li>
+						<li><a href="#">Pisos alquiler</a></li>
+					</ul>
+				</div>
+				<div class="col-12 col-sm-6 col-lg-3">
+					<ul class="menu">
+						<li>Transport</li>
+						<li><a href="#">Coches</a></li>
+						<li><a href="#">Barcos</a></li>
+						<li><a href="#">Yachts brokerage</a></li>
+						<li><a href="#">Aviones</a></li>
+						<li><a href="#">Drivers</a></li>
+					</ul>
+				</div>
+				<div class="col-12 col-sm-6 col-lg-3">
+					<ul class="menu">
+						<li>Events</li>
+						<li><a href="#">Boda's</a></li>
+						<li><a href="#">Cumpleaños</a></li>
+						<li><a href="#">Party’s</a></li>
+						<li><a href="#">Despedidas de solter@s</a></li>
+					</ul>
+				</div>
+				<div class="col-12 col-sm-6 col-lg-3">
+					<ul class="menu">
+						<li>Tailor</li>
+						<li><a href="#">VIP charting</a></li>
+						<li><a href="#">VIP discotecas</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row copyright">
+				<div class="col-12">
+					<p><strong>Ibiza Essence</strong> 2025. Todos los derechos reservados</p>
+					<p><a href="">Términos y condiciones</a> • <a href="">Política de privacidad</a></p>
+				</div>
+			</div>
+		</div>
+	</footer>
+
 	<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> -->
 	<script src="assets/js/bootstrap.bundle.min.js"></script>
 </body>
